f9a87ba41903b86660b463fa9d8ee8e67657d36a
unknown <nivzme@gmail.com>
2025-02-12 17:30:01 +0530

    Hosting

--- db/config.js
+++ db/config.js
@@ -1,23 +1,17 @@
-// /config/db.js
 const mongoose = require("mongoose");
 
-const uri = process.env.MONGO_URI; // Fetch from environment variable
+const uri = process.env.MONGO_URI
 
 const connectDB = async () => {
     try {
         await mongoose.connect(uri, {
-            serverSelectionTimeoutMS: 50000, // 50s server selection timeout
-            socketTimeoutMS: 45000, // 45s socket timeout
-            
         });
 
-        console.log("✅ MongoDB Connected Successfully!");
+        console.log("MongoDB Connected Successfully!");
     } catch (error) {
-        console.error("❌ MongoDB Connection Error:", error);
+        console.error("MongoDB Connection Error:", error);
         process.exit(1);
     }
 };
-
-
 
 module.exports = connectDB;
