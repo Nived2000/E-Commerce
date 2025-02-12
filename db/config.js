cc217b89b9506ef0b1b0b239ae64ed97e34ba376
Nived Geethak <91006526+Nived2000@users.noreply.github.com>
2025-02-12 23:24:15 +0530

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const uri = "mongodb://127.0.0.1:27017/eCommerceDB"
+const uri = "mongodb://13.60.20.41:27017/eCommerceDB"
 
 const connectDB = async () => {
     try {
