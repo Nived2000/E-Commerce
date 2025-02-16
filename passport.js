3147d1f723cf622dab111a033151fd42915be645
unknown <nivzme@gmail.com>
2025-02-16 23:43:45 +0530

    google ID done

--- passport.js
+++ passport.js
@@ -5,7 +5,7 @@ const User = require('./model/userModel');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "https://ecom-multi.shop/auth/callback",  
+  callbackURL: "https://ecom-multi.shop/user/auth/callback",  
 }, async (token, tokenSecret, profile, done) => {
   try {
 
